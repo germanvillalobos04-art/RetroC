5b1382bf64b9df08cd7c8616dd32505e01bb3280
germanvillalobos04-art <german.villalobos04@gmail.com>
2026-09-16 06:51:21 -0300

    Create games.js

---
+++ games.js
@@ -0,0 +1,8 @@
+const GAMES = {
+  "doom": { title: "DOOM", rom: "./roms/doom.sfc", system: "snes" },
+  "mk2":  { title: "Mortal Kombat II", rom: "./roms/mk2.sfc", system: "snes" },
+  "dkc1": { title: "Donkey Kong Country", rom: "./roms/dkc1.sfc", system: "snes" },
+  "dkc2": { title: "Donkey Kong Country 2", rom: "./roms/dkc2.sfc", system: "snes" },
+  "dkc3": { title: "Donkey Kong Country 3", rom: "./roms/dkc3.sfc", system: "snes" },
+  "ki":   { title: "Killer Instinct", rom: "./roms/ki.sfc", system: "snes" }
+};
