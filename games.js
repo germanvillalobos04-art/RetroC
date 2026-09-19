c4f7295994b3f17ad96abd735b9d346609b4ccaa
germanvillalobos04-art <german.villalobos04@gmail.com>
2026-09-19 12:00:11 -0300

    Update games.js

--- games.js
+++ games.js
@@ -1,8 +1,8 @@
 const GAMES = {
-  "doom": { title: "DOOM", rom: "./roms/doom.sfc", system: "snes" },
-  "mk2":  { title: "Mortal Kombat II", rom: "./roms/mk2.sfc", system: "snes" },
-  "dkc1": { title: "Donkey Kong Country", rom: "./roms/dkc1.smc", system: "snes" },
-  "dkc2": { title: "Donkey Kong Country 2", rom: "./roms/dkc2.sfc", system: "snes" },
-  "dkc3": { title: "Donkey Kong Country 3", rom: "./roms/dkc3.sfc", system: "snes" },
-  "ki":   { title: "Killer Instinct", rom: "./roms/ki.sfc", system: "snes" }
+  "doom": { title: "DOOM", rom: "./roms/doom.sfc", system: "snes", icon: "./icons/doom.png" },
+  "mk2":  { title: "Mortal Kombat II", rom: "./roms/mk2.sfc", system: "snes", icon: "./icons/mk2.png" },
+  "dkc1": { title: "Donkey Kong Country", rom: "./roms/dkc1.sfc", system: "snes", icon: "./icons/dkc1.png" },
+  "dkc2": { title: "Donkey Kong Country 2", rom: "./roms/dkc2.sfc", system: "snes", icon: "./icons/dkc2.png" },
+  "dkc3": { title: "Donkey Kong Country 3", rom: "./roms/dkc3.sfc", system: "snes", icon: "./icons/dkc3.png" },
+  "ki":   { title: "Killer Instinct", rom: "./roms/ki.sfc", system: "snes", icon: "./icons/ki.png" }
 };
